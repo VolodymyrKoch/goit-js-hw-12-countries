f44aeebda9dfa4b41f5788fd66a82c9a0313ec06
VolodymyrKoch <volodymyrkocherhan@gmail.com>
2020-11-04 16:17:33 +0200

    виправлення

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,61 +1,6 @@
-import { data } from "autoprefixer";
-import template from "./templates/template.hbs"
+export default function fetchCountries(searchQuery) { 
+  return fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`)
+    .then((data) => data.json())
+   
 
-import { alert, info } from '@pnotify/core';
-import '@pnotify/core/dist/PNotify.css';
-import { list } from "postcss";
-
-export default function fetchCountries() { 
-  const input = document.querySelector('.input')
-  const container = document.querySelector('.container')
-  const listRef = document.querySelector('.containerList')
-  
-  const debounce = require("lodash.debounce");
-
-  const countries = function (e) {
-    listRef.innerHTML = ""
-     e.target.value.length >= 2 ? fetch(`https://restcountries.eu/rest/v2/name/${e.target.value}`)
-      .then((data) => data.json())
-      .then((array) => {
-        console.log(array.length);
-       
-        if (array.length > 10) { 
-           alert('Too many matches found. Please enter a more specific query!')
-        }
-
-        array.forEach((el) => {
-          if (array.length >= 2 && array.length < 10) {
-
-            listRef.innerHTML += `<li class=listTemplate>${el.name}</li>`
-            
-            const listTemplate = document.querySelectorAll('.listTemplate')
-            listTemplate.forEach((el) => { 
-              const replacement = () => { input.value = el.textContent 
-               listRef.innerHTML=""
-               fetch(`https://restcountries.eu/rest/v2/name/${e.target.value}`)
-                .then((data) => data.json())
-                .then((array) => {listRef.insertAdjacentHTML('beforeend',template(array)) })
-              }
-            el.addEventListener('click', replacement);
-            })
-          }
-          if (array.length === 1) {
-             listRef.innerHTML=""
-            listRef.insertAdjacentHTML('beforeend', template(array))
-          }
-          
-      })
-       
-  })
-      .catch(error => {
-        info('Such a country is not found!')
-    })
-            : listRef.innerHTML = "";
-  };
-  
-  input.addEventListener(
-  'input',
-    debounce((e) => {
-      countries(e)}, 500),
-  );
-  }
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,76 @@
 import css from "./css/style.css";
-import searchQuery from "./fetchCountries.js";
+import fetchCountries from './fetchCountries.js';
 
+import template from "./templates/template.hbs"
 
+import { alert, info } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
 
-searchQuery()
+const input = document.querySelector('.input')
+const listRef = document.querySelector('.containerList')
+const debounce = require("lodash.debounce");
+
+const state = {
+  html: localStorage.getItem('html'),
+  input: localStorage.getItem('input')
+} 
+if (state.html || state.input) {
+  listRef.innerHTML = state.html;
+  input.value = state.input;
+}
+const countries = function (e) {
+      listRef.innerHTML = ""
+  if (e.target.value.length === 0) {
+    return
+   } 
+       fetchCountries(e.target.value)
+          .then((array) => {
+             if (array.length === 0) {
+              listRef.innerHTML = "";
+              info('Such a country is not found!')
+              return
+            }     
+            if (array.length > 10) { 
+          listRef.innerHTML = "";
+          alert('Too many matches found. Please enter a more specific query!')
+          return
+            }
+            if (array.length >= 2 && array.length < 10) {
+              listRef.innerHTML = "";
+              array.forEach((el) => { 
+                listRef.innerHTML += `<li class=listTemplate>${el.name}</li>`
+              })
+              return
+             }
+            if (array.length === 1) { 
+              listRef.innerHTML = "";
+               listRef.insertAdjacentHTML('beforeend', template(array[0]))
+            }
+         })
+        .catch(alert => {
+             info('Сервер не відповідає!')
+    })
+            
+};
+  input.addEventListener(
+  'input',
+    debounce((e) => {
+      countries(e)}, 500),
+);
+const countryChoice = function (e) { 
+    if (e.target.classList.contains('listTemplate')) {
+    input.value = e.target.textContent
+    fetchCountries(input.value)
+    .then(array => {
+      listRef.innerHTML = "";
+      listRef.insertAdjacentHTML('beforeend', template(array[0]))
+    })
+  }
+ }
+listRef.addEventListener('click', countryChoice)
+
+const beforeСlosing = function () {
+  localStorage.setItem('html', listRef.innerHTML);
+  localStorage.setItem('input', input.value);
+ }
+window.addEventListener('beforeunload', beforeСlosing)
